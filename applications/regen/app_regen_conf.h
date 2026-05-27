5499173bb74326b6a32f6312de2be51e6cfc4fed
Karl von Ladach <kalvacha@gmail.com>
2026-05-27 11:41:31 +0200

    changed number of pedal magnets to 18

--- applications/regen/app_regen_conf.h
+++ applications/regen/app_regen_conf.h
@@ -36,7 +36,7 @@
 #define APP_CUSTOM_CONF_PEDAL_SENSOR_PIN1        HW_HALL_ENC_PIN1
 #define APP_CUSTOM_CONF_PEDAL_SENSOR_PORT2       HW_HALL_ENC_GPIO2
 #define APP_CUSTOM_CONF_PEDAL_SENSOR_PIN2        HW_HALL_ENC_PIN2
-#define APP_CUSTOM_CONF_PEDAL_SENSOR_MAGNETS      24u    // including "virtual magnets"
+#define APP_CUSTOM_CONF_PEDAL_SENSOR_MAGNETS      18u    // including "virtual magnets"
 #define PEDAL_SENSOR_MAX_MAGNETS                  36u    // maximum number of magnets supported by the code, used for array sizing
 #define APP_CUSTOM_CONF_PEDAL_SENSOR_FILTER        0.8   // Range 0.0 to 1.0, where 1.0 gives the unfiltered value.
 #define APP_CUSTOM_CONF_PEDAL_AVG_ABOVE_RPM       10.0f  // CRPM - average last two samples above this value
